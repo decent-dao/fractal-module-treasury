ef404e2a08d5ff2b3acf89312e065cb9ed1aa566
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-16 08:39:16 -0400

    create2 pattern can predict

--- contracts/TreasuryModuleFactory.sol
+++ contracts/TreasuryModuleFactory.sol
@@ -3,14 +3,22 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/utils/Create2.sol";
 
 import "@fractal-framework/core-contracts/contracts/ModuleFactoryBase.sol";
 import "./interfaces/ITreasuryModuleFactory.sol";
 import "./interfaces/ITreasuryModule.sol";
 
 /// @notice A factory contract for deploying Treasury Modules
-contract TreasuryModuleFactory is ERC165, ModuleFactoryBase, ITreasuryModuleFactory {
-    event TreasuryCreated(address indexed treasuryAddress, address indexed accessControl);
+contract TreasuryModuleFactory is
+    ERC165,
+    ModuleFactoryBase,
+    ITreasuryModuleFactory
+{
+    event TreasuryCreated(
+        address indexed treasuryAddress,
+        address indexed accessControl
+    );
 
     function initialize() external initializer {
         __initFactoryBase();
@@ -25,19 +33,20 @@ contract TreasuryModuleFactory is ERC165, ModuleFactoryBase, ITreasuryModuleFact
         returns (address[] memory)
     {
         address[] memory createdContracts = new address[](1);
-      
+
         address accessControl = abi.decode(data[0], (address));
         address treasuryImplementation = abi.decode(data[1], (address));
+        bytes32 salt = abi.decode(data[2], (bytes32));
 
-        createdContracts[0] = address(
-            new ERC1967Proxy(
-                treasuryImplementation,
-                abi.encodeWithSelector(
-                    ITreasuryModule(payable(address(0))).initialize.selector,
-                    accessControl
-                )
+        createdContracts[0] = Create2.deploy(
+            0,
+            keccak256(abi.encodePacked(tx.origin, block.chainid, salt)),
+            abi.encodePacked(
+                type(ERC1967Proxy).creationCode,
+                abi.encode(treasuryImplementation, "")
             )
         );
+        ITreasuryModule(payable(createdContracts[0])).initialize(accessControl);
 
         emit TreasuryCreated(createdContracts[0], accessControl);
 
